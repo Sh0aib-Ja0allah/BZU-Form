f6efda0903754616b0ed60e4e9c86687eae6b7fb
Shoaib Jadallah <110256562+Sh0aib-Ja0allah@users.noreply.github.com>
2023-06-02 14:44:35 +0300

    Update Form.tsx file
    validate the email TextField and make some changes for the axios puch function call

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -10,66 +10,105 @@ import axios from 'axios'
 
 function Form() {
 
-  const [users, setUsers] = useState([])
-
-  const changeHandler = ( e: any) => {
-
+  interface User {
+    name: string;
+    email: string;
+    phone: string;
+    organization: string;
+    position: string;
+    city: string;
   }
 
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     axios
-//        .post('iconnect api link', {
-//           name: name,
-//           email: email,
-//           phone: phone,
-//           organization: organization,
-//           position: position,
-//           city: city,
-//           
-//        })
-//        .then((res) => {
-//           setUsers((users) => [res.data, ...users]);
-//           setName('');
-//           setEmail('');
-//           setPhone('');
-//           setOrganization('');
-//           setPosition('');
-//           setCity('');
-//        })
-//        .catch((err) => {
-//           console.log(err.message);
-//        });
-//  };
-
+  const [users, setUsers] = useState<User[]>([])
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [organization, setOrganization] = useState('')
+  const [position, setPosition] = useState('')
+  const [city, setCity] = useState('')
   const [open,setOpen] = useState(false)
+  const [snackbarSeverity, setSnackbarSeverity] = useState<'success' | 'error'>('success')
+  const [snackbarMessage, setSnackbarMessage] = useState('Your data was successfully submitted.')
 
-  const handleClose = (event?: React.SyntheticEvent | Event,reason?: string) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    // trim() method used to remove the spaces from a string.
+    if (
+      name.trim() === '' ||
+      email.trim() === '' ||
+      phone.trim() === '' ||
+      organization.trim() === '' ||
+      position.trim() === '' ||
+      city.trim() === ''
+    ) {
+      setSnackbarSeverity('error')
+      setSnackbarMessage('Please fill in all the form fields.')
+    }else{
+      axios
+      .post('iconnect api link', {
+          name: name,
+          email: email,
+          phone: phone,
+          organization: organization,
+          position: position,
+          city: city,
+      })
+      .then((res) => {
+          setUsers((PrevUsers) => [res.data, ...PrevUsers])
+          setName('')
+          setEmail('')
+          setPhone('')
+          setOrganization('')
+          setPosition('')
+          setCity('')
+          setSnackbarSeverity('success')
+          setSnackbarMessage('Your data was successfully submitted.')
+      })
+      .catch((err) => {
+          console.log(err.message)
+          setSnackbarSeverity('error')
+          setSnackbarMessage('An error occurred. Please try again later.')
+      })
+    }
+    setOpen(true)
+ } // end handleSubmit function.
+
+  // close Snackbar function.
+  const handleClose = (
+    event?: React.SyntheticEvent | Event, 
+    reason?: string
+  ) => {
     if(reason === 'clickaway'){
       return
     }
+    setOpen(false)
   }
 
   return (
     <div className="FormComponent">
+      {/* BZU header logo. */}
       <div className="headerImage">
         <img className='headerLogo' src={headerLogo} alt="Birzeit University header logo" />
       </div>
-      <form action="" className="Form">
-        <TextField onChange={changeHandler} sx={{width: '350px', my: '15px'}} name='name' type='text' color='success' label='Your Full name' />
-        <TextField onChange={changeHandler} sx={{width: '350px', my: '15px'}} name='email' type='email' color='success' label='Your Email' />
-        <TextField onChange={changeHandler} sx={{width: '350px', my: '15px'}} name='phone' type='number' color='success' label='Your phone number' />
-        <TextField onChange={changeHandler} sx={{width: '350px', my: '15px'}} name='organization' type='text' color='success' label='Organization' />
-        <TextField onChange={changeHandler} sx={{width: '350px', my: '15px'}} name='position' type='text' color='success' label='Your position' />
-        <TextField onChange={changeHandler} sx={{width: '350px', my: '15px'}} name='city' type='text' color='success' label='City' />
+      <form action="" className="Form" onSubmit={handleSubmit}>
+        <TextField sx={{width: '350px', my: '15px'}} onChange={(e) => setName(e.target.value)} value={name} name='name' type='text' color='success' label='Your Full name' required />
+        <TextField 
+          sx={{width: '350px', my: '15px'}} 
+          onChange={(e) => setEmail(e.target.value)} value={email} 
+          inputProps={{pattern: '[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}$',}} 
+          name='email' type='email' color='success' label='Your Email' required />
+        <TextField sx={{width: '350px', my: '15px'}} onChange={(e) => setPhone(e.target.value)} value={phone} name='phone' type='number' color='success' label='Your phone number' required />
+        <TextField sx={{width: '350px', my: '15px'}} onChange={(e) => setOrganization(e.target.value)} value={organization} name='organization' type='text' color='success' label='Organization' required />
+        <TextField sx={{width: '350px', my: '15px'}} onChange={(e) => setPosition(e.target.value)} value={position} name='position' type='text' color='success' label='Your position' required />
+        <TextField sx={{width: '350px', my: '15px'}} onChange={(e) => setCity(e.target.value)} value={city} name='city' type='text' color='success' label='City' required />
         <Button onClick={() => setOpen(true)} 
         type='submit' variant='contained' size='large'
         sx={{my: '15px'}}>
           Submit
         </Button>
         <Snackbar open={open} autoHideDuration={6000} onClose={handleClose} >
-          <Alert onClose={handleClose} severity='success' >
-            Your data is successfully submitted.
+          <Alert onClose={handleClose} severity={snackbarSeverity} >
+            {snackbarMessage}
           </Alert>
         </Snackbar>
       </form>
